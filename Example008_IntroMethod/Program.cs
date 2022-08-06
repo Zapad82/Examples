db223e4219303ebfc75651b01c23e4c6095c4e72
Nail <strana39@yandex.ru>
2022-08-06 15:19:11 +0500

    Делаем код компактнее

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -12,7 +12,7 @@ int Max(int arg1, int arg2, int arg3)
 }
 int a1 = 15;
 int b1 = 21;
-int c1 = 39;
+int c1 = 3339;
 int a2 = 12;
 int b2 = 23;
 int c2 = 33;
@@ -20,9 +20,10 @@ int a3 = 13;
 int b3 = 2311;
 int c3 = 33;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//Упрощаем запись 
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 
 Console.WriteLine(max);
