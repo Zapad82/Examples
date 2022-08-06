dfd2c1a41ffc237da384ca69f668f85bdf1aac44
Nail <strana39@yandex.ru>
2022-08-06 15:47:34 +0500

    так выглядит код с массивом

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -10,20 +10,14 @@ int Max(int arg1, int arg2, int arg3)
     if (arg3 > result) result = arg3;
     return result;
 }
-int a1 = 15;
-int b1 = 21;
-int c1 = 3339;
-int a2 = 12;
-int b2 = 23;
-int c2 = 33;
-int a3 = 13;
-int b3 = 2311;
-int c3 = 33;
+//               0   1   2   3   4   5   6   7   8
+int [] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
 
-//int max1 = Max(a1, b1, c1);
-//int max2 = Max(a2, b2, c2);
-//int max3 = Max(a3, b3, c3);
-//Упрощаем запись, делаем код компактнее. То, что ниже - соответствует тому, что выше скрыто комментариями.
-int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+//Console.WriteLine(array[0]);
 
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
 Console.WriteLine(max);
